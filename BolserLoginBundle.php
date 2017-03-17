6779ef86116ea10cacb0f2d551922859f5f0e98b
MatthewBooth <kryten2k35@gmail.com>
2017-03-17 10:40:15 +0000

    Initial commit

---
+++ BolserLoginBundle.php
@@ -0,0 +1,9 @@
+<?php
+
+namespace Bolser\LoginBundle;
+
+use Symfony\Component\HttpKernel\Bundle\Bundle;
+
+class BolserLoginBundle extends Bundle
+{
+}
---
+++ Controller/DefaultController.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace Bolser\LoginBundle\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+
+class DefaultController extends Controller
+{
+    /**
+     * @Route("/")
+     */
+    public function indexAction()
+    {
+        return $this->render('BolserLoginBundle:Default:index.html.twig');
+    }
+}
